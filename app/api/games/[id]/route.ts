8227345ce14d0000ecfe22724827009005ac9070
Andrea García Valverde <andrea.garciavalverde.practicas@telefonica.com>
2026-01-29 11:46:26 +0100

    function PATCH - updateGame works properly

--- app/api/games/[id]/route.ts
+++ app/api/games/[id]/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
-import { getGameById } from "@/app/services-controller/games";
-import { Game } from "@/app/models/game";
+import {
+  getGameById,
+  deleteGame,
+  updateGame,
+} from "@/app/services-controller/games";
+import { Game, UpdateGame } from "@/app/models/game";
 
 // Recibe una petición y los params (un id por el que va a buscar)
 // params: { id: string } -- en este caso el id como es parte de la URL viene como string --> Necesito convertirlo
@@ -50,6 +54,154 @@ export async function GET(
     return NextResponse.json(game, { status: 200 });
   } catch (error: any) {
     console.error("Error en GETById", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+// la función de DELETE como va por param(id) lo añado a esta ruta
+// DELETE (number) ----> Devolverá Promise<boolean> --> True si se pudo borrar / False si no
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  // Recibe una petición y los params -> id para buscar. Una vez encontrado, se elimina si todo ok
+  // Aprox al get(id) --> Sigo su proceso de resolver promise, convertir su id a num, comprobar el num, borrar
+  const resolvedParams = await params;
+  console.log("RESOLVED ID ->", resolvedParams.id);
+  try {
+    console.log("Intentando borrar");
+    const id = Number(resolvedParams.id);
+    if (isNaN(id)) {
+      return NextResponse.json(
+        { error: "El id debe ser un number" },
+        { status: 400 },
+      );
+    }
+    if (id <= 0) {
+      return NextResponse.json(
+        { error: "El id debe ser un number positivo" },
+        { status: 400 },
+      );
+    }
+    console.log("borrando después de verificar id");
+    const isDeleted: boolean = await deleteGame(id);
+    console.log("resultado de isDeleted --->", isDeleted);
+
+    if (!isDeleted) {
+      return NextResponse.json(
+        { error: `No se ha encontrado el juego con id ${id}` },
+        { status: 404 },
+      );
+    }
+    // 204 No Content para casos de DELETE / PUT
+    // pero su return sería diferente - Sólo devolvería el código de confirmación:
+    // return new NextResponse(null, { status: 204 });
+    // Prefiero que mande el success code OK y un mensaje que pueda mostrar en el Front
+    return NextResponse.json(
+      {
+        success: isDeleted,
+        message: `El juego con id ${id} ha sido borrado con éxito`,
+      },
+      { status: 200 },
+    );
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+// 🟡 NOTAS: Visto que se repite la validación al hacer get y delete..
+// Igual debería realizar una función validate que ejecute esos ifs y así sólo ejecutarlo y quedar más limpia cada petición
+
+// Para hacer el UPDATE voy a usar PATCH que me permite modificar ALGUNOS campos. PUT obliga a reemplazar el objeto completo (aunque sólo haya cambiaod 1 campo)
+// Recibe una petición a un id. Sigo misma estructura anterior a POST
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  console.log("Entra la petición al patch");
+
+  // 1. Resuelvo params
+  const resolvedParams = await params;
+  try {
+    const id = Number(resolvedParams.id);
+    console.log(`----> Leyendo el id ${id}`); // ---> ok
+
+    // 2. Compruebo el id
+    if (isNaN(id)) {
+      return NextResponse.json(
+        { error: "El id debe ser un number" },
+        { status: 400 },
+      );
+    }
+    if (id <= 0) {
+      return NextResponse.json(
+        { error: "El id debe ser un number positivo" },
+        { status: 400 },
+      );
+    }
+    // 3. Compruebo que el jkuego existe ANTES de intentar actualizarlo
+    const game = await getGameById(id);
+    if (!game) {
+      return NextResponse.json(
+        { error: `No se ha encontrado el juego con id ${id}` },
+        { status: 404 },
+      );
+    }
+    // 4. Leo el body de la request --> No debería estar vacío (se manda SIN {})
+    // Anidé este otro try-catch porque me daba error al intentar forzar mandar sin {} y no sabía el origen
+    // Si está vacío, no le dejo mandar la info a updateGame -> Generaría error
+    let body;
+    try {
+      body = await request.json();
+    } catch (error: any) {
+      return NextResponse.json(
+        { error: "El body debe ser un JSON válido y no estar vacío" },
+        { status: 400 },
+      );
+    }
+    // 4.1 Si el body esta vacío (se manda SÓLO {}) -> que se haya dado a editar, pero no se edita nada / se borran TODOS los campos -> Devuelvo / muestro la ficha del juego
+    if (Object.keys(body).length === 0) {
+      console.log("No se ha modificado el body. Devuelvo el juego sin cambios");
+      return NextResponse.json(game, { status: 200 });
+    }
+    // 4.2 Si se modifica el campo name (tenía valor, pero ahora se borra) --> NO debería permitirlo, es un campo OBLIGATORIO
+    if (body.name !== undefined && (!body.name || body.name.trim() === "")) {
+      return NextResponse.json(
+        { error: "El campo name es obligatorio" },
+        { status: 400 },
+      );
+    }
+    // 4.3 Valido rating (definido como num 0-10), para que no se puede escribir un valor que no esté en ese rango
+    if (
+      body.rating !== undefined &&
+      (isNaN(body.rating) || body.rating < 0 || body.rating > 10)
+    ) {
+      return NextResponse.json(
+        { error: "El rating debe ser un número entre 0 y 10" },
+        { status: 400 },
+      );
+    }
+
+    console.log("----> Leyendo el body", body);
+
+    // 5. Preparo su model con los datos del body
+    const gameData: UpdateGame = {
+      name: body.name,
+      release_date: body.release_date || null,
+      players_num: body.players_num || null,
+      id_publisher: body.id_publisher || null,
+      cover_url: body.cover_url || null,
+      rating: body.rating || null,
+    };
+    // 5. Llamo al controller para actualizar el juego pasándole el id y los datos recogidos del body
+    const updatedGame = await updateGame(id, gameData);
+    console.log(`----> Devolviendo el juego actualizado ${updatedGame}`);
+
+    // 6. Retorno el juego modificado y un status ok
+    return NextResponse.json(updatedGame, { status: 200 });
+  } catch (error: any) {
+    console.log(error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
--- app/services-controller/games.ts
+++ app/services-controller/games.ts
@@ -1,17 +1,10 @@
 import pool from "@/app/lib/db";
 import { Game, CreateGame, UpdateGame } from "@/app/models/game";
 
-// Construyo la interfaz (model) usando los campos de la tabla
-// ❓ Debería llevármelo a un archivo separado en carpeta "models" ? Como se hace en Nexo
-// ✅ Prefiero separar información, asi que sí, creo su archivo en models > game.ts
-
-// Comienzo con el CRUD ----> ⚠️ De momento CREATE & GET más adelante UPDATE & DELETE
-
-// CREATE createGame(data) ---> Devolverá <Game> --> el juego creado
+// ✅ CREATE createGame(data) ---> Devolverá <Game> --> el juego creado
 export async function createGame(data: CreateGame): Promise<Game> {
   const { name, release_date, players_num, cover_url, rating, id_publisher } =
     data;
-  // ❓ Lo más limpio sería parametrizar la query ?
   // VALUES ($n) en vez de VALUES (${game.name}, ....) para proteger la inyeción de SQL
   //   --> $n corresponde a la posición del parámetro en la query
   // RETURNING * ---> RETORUNING se usa queries de actualizar, nueva inserción o eliminar
@@ -41,15 +34,7 @@ export async function createGame(data: CreateGame): Promise<Game> {
   ];
 
   try {
-    // No parametrizada:
-    //   const result = await pool.query(
-    //     `INSERT INTO games (name, release_date, players_num, cover_url, rating, id_publisher)
-    //     VALUES ($1, $2, $3, $4, $5, $6)
-    //     RETURNING *`,
-    //     [name, release_date, players_num, cover_url, rating, id_publisher]
-    //   );
     // Parametrizando la query, no "ensucio" la petición y la hace más segura frente a inyección de SQL
-    // Parametrizada:
     const result = await pool.query(query, values);
     return result.rows[0];
   } catch (error: any) {
@@ -73,12 +58,12 @@ export async function createGame(data: CreateGame): Promise<Game> {
   }
 }
 
-// READ ---- GetAll & GetById & GetByName
-// getAllGames() ---> Devolverá <Game[]> --> Array de juegos
-// getGameById(number) ---> Devolverá <Game | null> --> un juego (x id)
-// getGameByName(string) ---> Devolverá <Game | null> --> un juego exacto (x name)
+// ✅ READ ---- GetAll & GetById & GetByName
+// ✅ getAllGames() ---> Devolverá <Game[]> --> Array de juegos
+// ✅ getGameById(number) ---> Devolverá <Game | null> --> un juego (x id)
+// ❌getGameByName(string) ---> Devolverá <Game | null> --> un juego exacto (x name)
+// ✅ searchGameByName es más adecuado que el getName por su parcialidad de info
 export async function getAllGames(): Promise<Game[]> {
-  // Como antes, la parametrizo para hacerla más legible
   //   Aquí ya no uso RETURNING porque quiero ver TODOS los juegos y no modificar
   const query = `
     SELECT 
@@ -153,6 +138,7 @@ export async function getGameById(id: number): Promise<Game | null> {
 //     );
 //   }
 // }
+
 // La búsqueda PARCIAL para los usuarios (el buscador con autocompletado y sugerencias por nombres q puedan coincidir)
 // Devolverá un ARRAY de juegos que contienen "zelda" en el nombre
 // ❓ Esta función tendría que llevármela a lib ...
@@ -174,7 +160,7 @@ export async function searchGamesByName(searchTerm: string): Promise<Game[]> {
   // % ---> cualquier carácter
   const searchPattern = `%${searchTerm}%`;
   try {
-    const result = await pool.query(query, [searchPattern]);
+    const result = await pool.query(query, [searchPattern]); // Le paso el parámetro de búsqueda a la query
     return result.rows;
   } catch (error: any) {
     throw new Error(`Error al buscar juegos por nombre: ${error.message}`);
@@ -182,11 +168,111 @@ export async function searchGamesByName(searchTerm: string): Promise<Game[]> {
 }
 
 // UPDATE (number, data)----> Devolverá <Game | null> --> el juego actualizado o null
-// export async function updateGame(id: number, data: UpdateGame): Promise<Game | null> {
+// Recibe un id y data (los datos para actualizar)
+// Actualiza sus campos  en la fila que coincida su id_game = id
+// Devolverça ese juego actualizado o null si no existe (mistaken id / field / ...)
+export async function updateGame(
+  id: number,
+  data: UpdateGame,
+): Promise<Game | null> {
+  console.log("Entra en UPDATE?");
+  console.log("Iniciado data:", data);
+  // La query UPDATE debería ser dinámica en función de los campos que se modifiquen
+  // Los campos que se vayan modificando los iré almacenando en 2 arrays: fields para saber en qué campo y values qué datos
+  const values: any[] = [];
+  const fields: string[] = [];
+  let counterQuery = 1; // Contador para $n
 
-// }
-// DELETE (number) ----> Devolverá Promise<boolean> --> True si se pudo borrar / False si no
-// export async function deleteGame(id: number): Promise<boolean> {
-//   try {
-//   } catch (error: any) {}
-// }
+  // Aquí necesitaría verificar qué campos se están modificando, para pasar la info a la query
+  // Comienzo a ver qué y dónde se modifica. Llenando los arrays fields y values
+  if (data.name !== undefined) {
+    // fields --> campo name y necesita un num para $n (name = $1)
+    fields.push(`name = $${counterQuery++}`);
+    values.push(data.name); // A values le paso el dato recogido
+  }
+  if (data.release_date !== undefined) {
+    fields.push(`release_date = $${counterQuery++}`);
+    values.push(data.release_date);
+  }
+  if (data.players_num !== undefined) {
+    fields.push(`players_num = $${counterQuery++}`);
+    values.push(data.players_num);
+  }
+  if (data.id_publisher !== undefined) {
+    fields.push(`id_publisher = $${counterQuery++}`);
+    values.push(data.id_publisher);
+  }
+  if (data.cover_url !== undefined) {
+    fields.push(`cover_url = $${counterQuery++}`);
+    values.push(data.cover_url);
+  }
+  if (data.rating !== undefined) {
+    fields.push(`rating = $${counterQuery++}`);
+    values.push(data.rating);
+  }
+
+  console.log("fields", fields);
+  console.log("values", values);
+  // Campos que son fijos para la query (e ir probando):
+  //    - UPDATE tabla games    - SET (campos a modificar - dinámico)   - WHERE (condiciones - dinámico)   - RETURNING * (que devuelva los campos)
+  // Esta query es de prueba para hacer un cambio fijo (en name) e ir probando con postman en su ruta
+  //    1 --> Pruebo que cambie el nombre (SET name) por el valor recogido de la query (= $1), y el where que sea tb el recogido por la query (= $2)
+  //      -----> Aquí array contenía  values = [data.name, id]
+  //      -----> la query queda: UPDATE games SET name = $1 WHERE id_game = $2 RETURNING *
+
+  //    2 --> Cambio a query dinámica que recoge los fields en SET y los values en WHERE
+  //      -----> Aquí los arrays values & fields = []
+  //      -----> Necesito una variable que lleve la cuenta para $n --> un contador
+  //      -----> Para cada campo posible (de momento name) chequeo si se ha modificado:
+  //         ---> Si data.CAMPO !== undefined -> Si existe, se agrega a fields el numero de $n y se pasa el dato a values en ese if
+  //         ---> Fuera del if, a values le paso el id del juego a modificar porque sino
+  //              el where daba error
+  //      -----> Y la query queda: UPDATE games SET ${fields.join(',')} y WHERE id_game = $${counterQuery}
+
+  values.push(id); // ⚠️⚠️  IMPORTANTE: Pasar el id al VALUES, porque es el último param para la pool
+
+  //    3 --> Ahora que name ok, añado todos los ifs de todos los campos posibles, para construir la query completa
+  const query = `
+  UPDATE games
+  SET ${fields.join(", ")}
+  WHERE id_game = $${counterQuery}
+  RETURNING *
+  `;
+  console.log(query);
+  try {
+    console.log(`Juego con id ${id} y data: `, data);
+    // La info que le paso a la query recoge la query dinámica, y los valores modificados
+    const result = await pool.query(query, values);
+    console.log(`query --> ${query}, con value ---> ${values}`);
+
+    // Si no se pasa data (porque no se hayan modificado campos), devolverá null
+    return result.rows[0] || null;
+  } catch (error: any) {
+    // Contemplo errores posibles, mismos que al crear
+    if (error.code === "23505") {
+      throw new Error(`El juego "${data.name}" ya existe`);
+    }
+    if (error.code === "23503") {
+      throw new Error(`El publisher con id ${data.id_publisher} no existe`);
+    }
+    throw new Error(`Error al modificar el juego ${id}: ${error.message}`);
+  }
+}
+
+// ✅ DELETE (number) ----> Devolverá Promise<boolean> --> True si se pudo borrar / False si no
+export async function deleteGame(id: number): Promise<boolean> {
+  try {
+    const query = `
+      DELETE FROM games 
+      WHERE id_game = $1 
+      RETURNING id_game
+    `;
+    const result = await pool.query(query, [id]); // Le paso el ID a la query
+    console.log("Se está borrando?", result);
+
+    // Si se puede ejecutar, el rowCount debería estar vacío / not null y devolver true
+    return result.rowCount !== null && result.rowCount > 0;
+  } catch (error: any) {
+    throw new Error(`Error al borrar el juego ${id}: ${error.message}`);
+  }
+}
